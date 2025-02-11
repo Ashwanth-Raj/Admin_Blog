0186e095ece4a02271427337cea5492ba0d4d19e
ASHWANTH RAJ <142151581+Ashwanth-Raj@users.noreply.github.com>
2025-02-11 20:10:24 +0530

    baseurl axiosInstance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,9 +1,10 @@
 // src/utils/axiosInstance.js
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-});
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000'; // Default to local backend
+
+const axiosInstance = axios.create({ baseURL });
+
 
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token'); // Get token from localStorage
